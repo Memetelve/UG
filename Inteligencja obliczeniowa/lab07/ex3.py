d13745e7e2e53344ee25b7ca2e1908ad4e8d46bf
Memetelve <memetelve1@gmail.com>
2024-06-09 22:29:57 +0200

    :sparkles: feat: IO lab07 ex3

--- Inteligencja obliczeniowa/lab07/ex3.py
+++ Inteligencja obliczeniowa/lab07/ex3.py
@@ -1,1 +1,17 @@
-import twscrape
+from twscrape import API, gather
+import asyncio
+
+
+async def main():
+    api = API()
+
+    await api.pool.add_account("memetelve", "redacted", "", "")
+    await api.pool.login_all()
+    posts = await gather(api.search("python", limit=10))
+    for post in posts:
+        with open("posts.json", "a") as f:
+            f.write(post.json())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
